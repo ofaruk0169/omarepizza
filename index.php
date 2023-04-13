3866647518f42e3d9865be5fb6dba6ad9eaec8a1
Omare Faruk <ofaruk0169@gmail.com>
2023-04-13 20:52:25 +0100

    alt foreach syntax

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 
     //close connection
     mysqli_close($conn);
+
     
     
 ?>
@@ -33,14 +34,18 @@
     <h4 class="center" grey-text>Pizzas!</h4>
     <div class="container">
         <div class="row">
-            <?php foreach($pizzas as $pizza) { ?>
+            <?php foreach($pizzas as $pizza): { ?>
 
 
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                            <ul>
+                                <?php foreach(explode(',', $pizza['ingredients']) as $ing): { ?>
+                                    <li><?php echo htmlspecialchars($ing); ?></li>
+                                <?php endforeach; } ?>
+                            </ul>
                         </div>
                         <div class="card-action right-align">
                             <a href="#" class="brand-text">More Information</a>
@@ -48,7 +53,7 @@
                     </div>
                 </div>
 
-           <?php } ?>
+           <?php endforeach}; ?>
         </div>
     </div>
     <?php include('templates/footer.php'); ?>
