fbc0242e1ad3af216775db1bc4e296dfe254bcf1
Omare Faruk <ofaruk0169@gmail.com>
2023-04-13 19:41:03 +0100

    added the connection code to the database

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+    $conn = mysqli_connect('localhost', 'omare', 'test1234', 'omare_pizza');
+
+    if(!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
 ?>
 
 <!DOCTYPE html>
